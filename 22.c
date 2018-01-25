446ae2dab206ec4e711aad2545d24445a3761db5
arukumar12 <30829827+arukumar12@users.noreply.github.com>
2018-01-25 15:54:36 +0530

    Create 22.c

---
+++ 22.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a[100],n,m,i;
+	printf("\nEnter the size of the array: ");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	m=a[0];
+	for(i=0;i<n;i++)
+	{
+		if(a[i]>m)
+		{
+			max=a[i];
+		}
+	}
+	printf("\nThe maximum element is %d ",m);
+	return 0;
+}
